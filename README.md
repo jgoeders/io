827e4975a193885a0452f63b44236e5629a060af
Chris Kitras <chkitras@gmail.com>
2023-03-02 12:02:46 -0700

    init

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# io
+# I/O
+
+## Experience Summary
+
+### How many hours did it take to complete assignment?
+[Put answer here]
+
+### What did you enjoy about this lab?
+[Put answer here]
+
+### What were the major challenges you had with this lab? Try to be as detailed as possible.
+[Put answer here]
+
+## Lab Specific Tasks
+
+1. What are the file operation functions we use in this lab and what do they do?
+
+[Put answer here]
+
+
+2. What are the directory operation functions we use in this lab and what do they do?
+
+[Put answer here]
+
+3. What are the FILE and DIR pointers and how do they behave?
+
+[Put answer here]
+
+
+### Proof of Completion
+4. Upload a 2 minute video demonstrating the functionality of your project. Save it as `passoff.mp4`.
+
+![video](./passoff.mp4)
+
---
+++ main.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include "buttons.h"
+#include "device.h"
+#include "display.h"
+#include "log.h"
+#include "../fonts/fonts.h"
+
+/* DEFAULT COLORS */
+#define BACKGROUND_COLOR WHITE
+#define FONT_COLOR BLACK
+
+// Makes sure to deinitialize everything before program close
+void intHandler(int dummy) {
+    log_info("Exiting...");
+    display_exit();
+    exit(0);
+}
+
+int main(void) {
+
+    signal(SIGINT, intHandler);
+
+    // Initialize the hardware here
+    log_info("Starting...");
+    /* place lines here*/
+    delay_ms(1000);
+
+    // Add file scanning logic here
+    
+
+    // Everythin inside this loop will repeat until 'Ctrl-C' is pressed in the terminal.
+    // Add drawing logic here
+    while (true) {
+        delay_ms(200);
+
+        // // Example of how to interact with a button
+        // if (button_up() == 0) {
+        // // Do something upon detecting button press
+
+        //     while (button_up() == 0) {
+        //         // Do something while the button is pressed
+        //         delay_ms(1);
+        //     }
+        // }
+
+        /* Write code to fufill lab requirements here*/
+    }
+    return 0;
+}
